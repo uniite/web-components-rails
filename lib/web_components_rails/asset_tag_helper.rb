58d217466337abc1e7ee5eef88a4ae6a19afc2ed
uniite <jon@jbotelho.com>
2016-05-11 12:13:35 -0400

    Fix html_import_tag to work with sprockets-rails 3.x, and without better_errors

--- lib/web_components_rails/asset_tag_helper.rb
+++ lib/web_components_rails/asset_tag_helper.rb
@@ -6,10 +6,13 @@ module WebComponentsRails::AssetTagHelper
     debug_mode = options['debug'] != false && request_debug_assets?
 
     sources.map do |source|
-      check_errors_for(source, type: :html)
+      # Optional; provided by better_errors
+      if respond_to?(:check_errors_for)
+        check_errors_for(source, type: :html)
+      end
       # In debug mode, include each html asset separately (if we can find the asset)
       if debug_mode
-        asset = lookup_asset_for_path(source, type: :html)
+        asset = lookup_html_debug_asset(source)
         asset.to_a.map do |a|
           _html_import_tag(path_to_asset(a.logical_path, path_options.merge(debug: true)))
         end
@@ -23,11 +26,20 @@ module WebComponentsRails::AssetTagHelper
 
   private
 
-  def _html_import_tag(source, path_options = {})
-    tag_options = {
-      rel: 'import',
-      href: path_to_asset(source, path_options.merge(extname: '.html', type: :html))
-    }
-    tag(:link, tag_options)
-  end
+    def _html_import_tag(source, path_options = {})
+      tag_options = {
+        rel: 'import',
+        href: path_to_asset(source, path_options.merge(extname: '.html', type: :html))
+      }
+      tag(:link, tag_options)
+    end
+
+    def lookup_html_debug_asset(path)
+      path = path_with_extname(path, type: :html)
+
+      resolve_asset do |resolver|
+        resolver.find_debug_asset path
+      end
+    end
+    
 end
