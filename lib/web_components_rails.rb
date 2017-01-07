8f085613aa523f4bb8ff5205d8283d3237d7150b
Jon Botelho <jon@jbotelho.com>
2017-01-07 14:29:55 -0500

    Remove broken haml require

--- lib/web_components_rails.rb
+++ lib/web_components_rails.rb
@@ -5,7 +5,6 @@ module WebComponentsRails
 end
 
 require 'web_components_rails/asset_tag_helper'
-require 'web_components_rails/haml_template'
 require 'web_components_rails/html_import_processor'
 require 'web_components_rails/railtie'
 require 'web_components_rails/version'
